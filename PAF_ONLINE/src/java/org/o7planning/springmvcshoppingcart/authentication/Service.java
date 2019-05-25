652806730d0abaed48f407acdc2e9b3620f2730e
itpmlearn <47427215+itpmlearn@users.noreply.github.com>
2019-05-25 22:51:12 +0530

    Add files via upload

---
+++ PAF_ONLINE/src/java/org/o7planning/springmvcshoppingcart/authentication/Service.java
@@ -0,0 +1,5 @@
+package java.org.o7planning.springmvcshoppingcart.authentication;
+
+public @interface Service {
+
+}
---
+++ PAF_ONLINE/src/java/org/o7planning/springmvcshoppingcart/config/SppringSecuritiInitializer.java
@@ -0,0 +1,10 @@
+package org.o7planning.springmvcshoppingcart.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+
+public class SppringSecuritiInitializer extends AbstractSecurityWebApplicationInitializer {
+
+   // Do nothing
+
+}
